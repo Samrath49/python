dc7fb04e5bcc2f0fec40d267bfe14028ca598930
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2020-06-01 17:26:00 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,9 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> inputs = [1.2,3.1,5.5]
+>>> weights = [3.1,2.1,8.7]
+>>> bias = 3
+>>> output=inputs[0]*weights[0]+inputs[1]*weights[1]+inputs[2]*weights[2]+bias
+>>> print(output)
+61.08
+>>> 
